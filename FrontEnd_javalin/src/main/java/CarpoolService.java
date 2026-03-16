abdfcc6fef087979d6d83b91db6a211a361138e3
Elias Teis Christensen <48584591+Grottenhofnar@users.noreply.github.com>
2026-03-16 10:42:28 +0100

    Add files via upload

--- FrontEnd_javalin/src/main/java/CarpoolService.java
+++ FrontEnd_javalin/src/main/java/CarpoolService.java
@@ -1,2 +1,46 @@
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class CarpoolService {
+    static File file = new File("rides.json");
+    static ObjectMapper mapper = new ObjectMapper();
+
+
+    public static void main(String[] args) throws IOException {
+        Javalin app = Javalin.create(config ->{
+            config.staticFiles.add("/public", Location.CLASSPATH);
+        } );
+
+
+        if(!file.exists()){
+            mapper.writeValue(file, new ArrayList<>());
+        }
+
+        app.get("/rides", ctx -> {
+            List<Map> rides = mapper.readValue(file, List.class);
+            StringBuilder html = new StringBuilder();
+            html.append()
+
+        });
+
+        app.post("/rides", ctx->{
+            List<Map> rides = mapper.readValue(file, List.class);
+            rides.add(ctx.bodyAsClass(Map.class));
+            mapper.writeValue(file, rides);
+            ctx.status(201);
+        });
+        app.start();
+    }
+
+
+
+
 }
---
+++ FrontEnd_javalin/target/classes/public/dynamicStyling.js
@@ -0,0 +1,6 @@
+
+if (role === "passenger") {
+    document.body.classList.add("passenger");
+} else if (role === "driver") {
+    document.body.classList.add("driver");
+}
